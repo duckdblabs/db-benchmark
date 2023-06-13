333f3600142d6c9f1c1ff618d30cacb8554eb590
Daniël Heres <daniel.heres@coralogix.com>
2023-06-13 22:38:36 +0200

    Fix version script

--- datafusion/ver-datafusion.sh
+++ datafusion/ver-datafusion.sh
@@ -1,1 +1,2 @@
-echo "0.4.0" > datafusion/VERSION
+source ./datafusion/py-datafusion/bin/activate
+python3 -c 'import datafusion as df; open("datafusion/VERSION","w").write(df.__version__); open("datafusion/REVISION","w").write("");' > /dev/null
