b104f3a99e333bec33b565b8d9975b261f1268a7
jangorecki <j.gorecki@wit.edu.pl>
2023-08-09 16:16:03 +0200

    more pkgs required for dplyr script

--- dplyr/setup-dplyr.sh
+++ dplyr/setup-dplyr.sh
@@ -6,3 +6,13 @@ mkdir -p ./dplyr/r-dplyr
 Rscript -e 'install.packages("dplyr", lib="./dplyr/r-dplyr")'
 Rscript -e 'install.packages("slider", lib="./dplyr/r-dplyr")' ## rollfun
 Rscript -e 'install.packages("data.table", lib="./dplyr/r-dplyr")' ## data load
+Rscript -e 'install.packages("rlang", lib="./dplyr/r-dplyr")' ## seems also be needed
+Rscript -e 'install.packages("R6", lib="./dplyr/r-dplyr")' ## seems also be needed
+Rscript -e 'install.packages("glue", lib="./dplyr/r-dplyr")' ## seems also be needed
+Rscript -e 'install.packages("lifecycle", lib="./dplyr/r-dplyr")' ## seems also be needed
+Rscript -e 'install.packages("magrittr", lib="./dplyr/r-dplyr")' ## seems also be needed
+Rscript -e 'install.packages("cli", lib="./dplyr/r-dplyr")' ## seems also be needed
+Rscript -e 'install.packages("fansi", lib="./dplyr/r-dplyr")' ## seems also be needed
+Rscript -e 'install.packages("utf8", lib="./dplyr/r-dplyr")' ## seems also be needed
+Rscript -e 'install.packages("pkgconfig", lib="./dplyr/r-dplyr")' ## seems also be needed
+
