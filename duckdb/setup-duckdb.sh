c11acc9bae2869ce994176ea7d90c02d83f967af
Tom Ebergen <tom@ebergen.com>
2023-10-09 11:29:20 +0200

    fix setup script

--- duckdb/setup-duckdb.sh
+++ duckdb/setup-duckdb.sh
@@ -2,9 +2,18 @@
 set -e
 
 # install stable duckdb
-mkdir -p ./duckdb/r-duckdb
-# instal duckdb 0.8.1-3
-# TODO: update to 0.9.0
-Rscript -e 'install.packages("https://cran.r-project.org/src/contrib/duckdb_0.8.1-3.tar.gz", repos="https://cloud.r-project.org/", lib="./duckdb/r-duckdb")'
+mkdir -p ./duckdb-latest/r-duckdb-latest
+# Rscript -e  'withr::with_libpaths(new = "./duckdb-latest/r-duckdb-latest", devtools::install_github("duckdb/duckdb/tools/rpkg"))'
 # prevent errors when running 'ver-duckdb.sh'
-Rscript -e 'install.packages("DBI", lib="./duckdb/r-duckdb")'
+Rscript -e 'install.packages("DBI", lib="./duckdb-latest/r-duckdb-latest", repos = "http://cran.us.r-project.org")'
+
+
+cd duckdb-latest
+git clone https://github.com/duckdb/duckdb-r.git
+cd duckdb-r
+git checkout v0.8.1-3
+cd ..
+ncores=`python3 -c 'import multiprocessing as mp; print(mp.cpu_count())'`
+MAKE="make -j$ncores" R CMD INSTALL -l "./r-duckdb-latest" duckdb-r
+rm -rf duckdb-r
+cd ..
