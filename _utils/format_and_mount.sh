d94e2ba25274674503864607d013dc1f7b3663a8
Tmonster <tom@ebergen.com>
2023-10-24 15:59:11 +0200

    format and mount script start. need to add proper datagen still

--- _utils/format_and_mount.sh
+++ _utils/format_and_mount.sh
@@ -24,16 +24,17 @@ cp ~/db-benchmark/data/*.csv .
 source path.env && python3 _utils/install_all_solutions.py all
 
 
-# setup mount for clickhouse extra data
+# setup mount for clickhouse spill
 sudo mkfs -t xfs /dev/nvme1n1
 sudo mkdir /var/lib/clickhouse-nvme-mount/
 sudo mount /dev/nvme1n1 /var/lib/clickhouse-nvme-mount/
+# not sure if below is necessary.
 sudo cp -a /var/lib/clickhouse/. /var/lib/clickhouse-nvme-mount/
 # change ownership of new mount to clickhouse
 sudo chown -R clickhouse:clickhouse /var/lib/clickhouse-nvme-mount/
 sudo chown -R clickhouse:clickhouse /dev/nvme1n1
-sudo chmod 777 /home/ubuntu/
 
+# add config so clickhouse knows to use the mount to spill data
 sudo cp clickhouse/clickhouse-mount-config.xml /etc/clickhouse-server/config.d/data-paths.xml
 
 echo "------------------------------------------"
