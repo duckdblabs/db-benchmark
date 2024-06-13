63609a90ba1f681e21cbbb4c60746274f1a1d3b2
Tmonster <tom@ebergen.com>
2024-06-13 15:23:43 +0000

    remove duckdb latest. add note to why polars result is removed

--- _report/report.R
+++ _report/report.R
@@ -249,7 +249,10 @@ time_logs = function(path=getwd()) {
   lt <- load_time(path=getwd())
 
   ct = clean_time(lt)
+  # https://github.com/pola-rs/polars/issues/16937
   ct = ct %>% filter(!(solution == 'polars' & question == 'sum v3 count by id1:id6'))
+  # remove duckdb-latest for now
+  ct = ct %>% filter(!(solution == 'duckdb-latest'))
   d = model_time(ct)
   ll <- load_logs(path=path)
   ll$solution[ll$solution == "arrow"] <- "R-arrow"
