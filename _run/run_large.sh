bfaedbbf06f28581dd33c7aded3b4639f9297711
Tmonster <tom@ebergen.com>
2025-10-10 13:47:19 +0200

    add a publish report workflow

--- _run/run_large.sh
+++ _run/run_large.sh
@@ -3,26 +3,7 @@
 rm data/*.csv
 rm data/*.duckdb
 
-# get groupby large (50GB datasets)
-aws s3 cp s3://duckdb-data-for-ec2-regression-tests/db-benchmark-data/groupby_large.duckdb data/groupby_large.duckdb
-# get join small (50GB datasets)
-aws s3 cp s3://duckdb-data-for-ec2-regression-tests/db-benchmark-data/join_large.duckdb data/join_large.duckdb
-
-
-# expand groupby-large datasets to csv
-duckdb data/groupby_large.duckdb  -c "copy G1_1e9_1e2_0_0 to 'data/G1_1e9_1e2_0_0.csv' (FORMAT CSV)"
-duckdb data/groupby_large.duckdb  -c "copy G1_1e9_1e1_0_0 to 'data/G1_1e9_1e1_0_0.csv' (FORMAT CSV)"
-duckdb data/groupby_large.duckdb  -c "copy G1_1e9_2e0_0_0 to 'data/G1_1e9_2e0_0_0.csv' (FORMAT CSV)"
-duckdb data/groupby_large.duckdb  -c "copy G1_1e9_1e2_0_1 to 'data/G1_1e9_1e2_0_1.csv' (FORMAT CSV)"
-duckdb data/groupby_large.duckdb  -c "copy G1_1e9_1e2_5_0 to 'data/G1_1e9_1e2_5_0.csv' (FORMAT CSV)"
-
-
-# expand join-large datasets to csv
-duckdb data/join_large.duckdb  -c "copy J1_1e9_NA_0_0 to 'data/J1_1e9_NA_0_0.csv' (FORMAT CSV)"
-duckdb data/join_large.duckdb  -c "copy J1_1e9_1e9_0_0 to 'data/J1_1e9_1e9_0_0.csv' (FORMAT CSV)"
-duckdb data/join_large.duckdb  -c "copy J1_1e9_1e6_0_0 to 'data/J1_1e9_1e6_0_0.csv' (FORMAT CSV)"
-duckdb data/join_large.duckdb  -c "copy J1_1e9_1e3_0_0 to 'data/J1_1e9_1e3_0_0.csv' (FORMAT CSV)"
-
+./_run/download_large_data.sh
 
 cp _control/data_large.csv _control/data.csv
 
--- collapse/setup-collapse.sh
+++ collapse/setup-collapse.sh
@@ -3,6 +3,7 @@ set -e
 
 # install stable collapse
 mkdir -p ./collapse/r-collapse
-Rscript -e 'install.packages(c("Rcpp", "collapse"), lib="./collapse/r-collapse", repos = "http://cloud.r-project.org")'
+ncores=`python3 -c 'import multiprocessing as mp; print(mp.cpu_count())'`
+MAKEFLAGS="-j$ncores" Rscript -e 'install.packages(c("Rcpp", "collapse"), lib="./collapse/r-collapse", repos = "http://cloud.r-project.org")'
 
 ./collapse/ver-collapse.sh
