b7cc8152d8e478ea78db6762a9a605c912ee5339
Michael Chavinda <mschavinda@gmail.com>
2026-01-03 08:11:42 -0800

    fix: Source ghcup encironment before calling it.

--- haskell/setup-haskell.sh
+++ haskell/setup-haskell.sh
@@ -5,6 +5,7 @@ set -e
 if ! command -v ghcup &> /dev/null; then
     echo "Installing Cabal..."
     curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 BOOTSTRAP_HASKELL_MINIMAL=1 sh
+    source $HOME/.ghcup/env
     ghcup install cabal
 fi
 
