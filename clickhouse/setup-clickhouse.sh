04fd0e139ce7cf39ca068f68128551d5a989e0da
Tmonster <tom@ebergen.com>
2026-04-11 13:54:26 -0700

    blah

--- clickhouse/setup-clickhouse.sh
+++ clickhouse/setup-clickhouse.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 # install
-curl https://clickhouse.com/install.sh | sudo sh
+cd clickhouse
 
-# stop server if service was already running
-sudo service clickhouse-server start ||:
-
-
-# modify clickhouse settings so data is stored on the mount.
 sudo mkdir -p /var/lib/mount/clickhouse-nvme-mount/
+
+rm -rf clickhouse
+curl https://clickhouse.com/install.sh | sudo sh
+expect -c '
+  spawn sudo ./clickhouse install
+  expect "password"
+  send "\r"
+  expect eof
+'
+
+# created by install script above
+sudo chown -R clickhouse:clickhouse '/etc/clickhouse-server'
+
+# copy clickhouse config so install will inherit the correct data path
+# there are questions as to what happens on a fresh install.
+sudo cp clickhouse-mount-config.xml /etc/clickhouse-server/config.d/data-paths.xml
+
+# modify mount so clickhouse can read it
 sudo chown -R clickhouse:clickhouse /var/lib/mount/clickhouse-nvme-mount
-sudo chown -R clickhouse:clickhouse /var/lib/mount/clickhouse-nvme-mount/store
 
 # set up clickhouse tmp space
 sudo mkdir -p /var/lib/mount/clickhouse-tmp/
 sudo chown clickhouse:clickhouse /var/lib/mount/clickhouse-tmp
 
-# copy clickhouse config
-sudo cp -a /var/lib/clickhouse/. /var/lib/mount/clickhouse-nvme-mount/
-sudo cp clickhouse/clickhouse-mount-config.xml /etc/clickhouse-server/config.d/data-paths.xml
-
-
 # start server
-sudo rm -rf /var/log/clickhouse-server/clickhouse-server.err.log /var/log/clickhouse-server/clickhouse-server.log
-sudo service clickhouse-server start
-
+echo "attempting to start server"
+sudo clickhouse start
+echo "finished starting clickhouse server"
 
 MEMORY_LIMIT=0
 BYTES_BEFORE_EXTERNAL_GROUP_BY=0
@@ -31,6 +38,8 @@ if [[ $MACHINE_TYPE == "c6id.4xlarge" ]]; then
 	MEMORY_LIMIT=28000000000
 	BYTES_BEFORE_EXTERNAL_GROUP_BY=20000000000
 fi
+
+cd ..
 
 clickhouse-client --query "CREATE USER IF NOT EXISTS db_benchmark IDENTIFIED WITH no_password SETTINGS max_memory_usage = $MEMORY_LIMIT, max_bytes_before_external_group_by = $BYTES_BEFORE_EXTERNAL_GROUP_BY WRITABLE;"
 clickhouse-client --query "GRANT select, insert, create, alter, alter user, create table, truncate, drop, system flush logs on *.* to db_benchmark;"
--- clickhouse/ver-clickhouse.sh
+++ clickhouse/ver-clickhouse.sh
@@ -3,7 +3,8 @@ set -e
 
 source ./clickhouse/ch.sh # clickhouse helper scripts
 
-ch_installed && clickhouse-client --version-clean > clickhouse/VERSION && echo "" > clickhouse/REVISION
+ch_installed && clickhouse-client --version-clean > clickhouse/VERSION
+echo "" > clickhouse/REVISION
 
 if [[ $TEST_RUN != "true" ]]; then
 	sudo chown ubuntu:ubuntu clickhouse/VERSION
