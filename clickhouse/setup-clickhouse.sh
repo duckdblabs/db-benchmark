86ace532b058333469fa1e7815cc99c22af05635
Tmonster <tom@ebergen.com>
2026-04-11 11:20:18 -0700

    make clickhouse install easier

--- clickhouse/setup-clickhouse.sh
+++ clickhouse/setup-clickhouse.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 # install
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
-curl -fsSL 'https://packages.clickhouse.com/rpm/lts/repodata/repomd.xml.key' | sudo gpg --dearmor -o /usr/share/keyrings/clickhouse-keyring.gpg
-
-ARCH=$(dpkg --print-architecture)
-echo "deb [signed-by=/usr/share/keyrings/clickhouse-keyring.gpg arch=${ARCH}] https://packages.clickhouse.com/deb stable main" | sudo tee /etc/apt/sources.list.d/clickhouse.list
-sudo apt-get update
-
-sudo apt-get install -y clickhouse-server clickhouse-client
+curl https://clickhouse.com/install.sh | sudo sh
 
 # stop server if service was already running
 sudo service clickhouse-server start ||:
