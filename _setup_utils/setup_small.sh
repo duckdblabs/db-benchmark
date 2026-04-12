f3efb6713a0cce58289c3a5cba0c0af9f8353b34
Tmonster <tom@ebergen.com>
2026-04-12 14:59:02 -0700

    fix some setup scripts

--- _setup_utils/setup_small.sh
+++ _setup_utils/setup_small.sh
@@ -16,9 +16,24 @@ sudo apt-get -qq install -y ca-certificates libtinfo5
 
 sudo apt-get install -y zlib1g-dev
 sudo apt-get install -y pandoc unzip
+sudo apt-get install -y cmake libuv1-dev
 
 # update virtualenv
 python3 -m pip install virtualenv
+
+# install duckdb
+curl https://install.duckdb.org | sh
+export PATH="/home/ubuntu/.duckdb/cli/latest":$PATH
+
+# install the aws command (X86)
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install
+
+# install the aws command (ARM64)
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install
 
 # sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 # sudo add-apt-repository "deb [arch=amd64,i386] https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
