3f8b501c5d2c1e324fe555ce4d07e12cdf406ff1
Tmonster <tom@ebergen.com>
2026-04-12 11:16:50 -0700

    fix julia scripts

--- juliadf/setup-juliadf.sh
+++ juliadf/setup-juliadf.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 # install julia
 
-wget -q https://julialang-s3.julialang.org/bin/linux/x64/1.10/julia-1.10.5-linux-x86_64.tar.gz
-tar -xvf julia-1.10.5-linux-x86_64.tar.gz > tar.out 2> tar.err
-sudo mv julia-1.10.5 /opt
-rm julia-1.10.5-linux-x86_64.tar.gz
-# put to paths
-echo 'export JULIA_HOME=/opt/julia-1.10.5' >> path.env
-echo 'export PATH=$PATH:$JULIA_HOME/bin' >> path.env
-# note that cron job must have path updated as well
+curl -fsSL https://install.julialang.org | sh -s -- -y
+. /home/ubuntu/.bashrc
 
-source path.env
 
 # install julia dataframes and csv packages
 julia -q -e 'using Pkg; Pkg.add(["DataFrames","CSV"])'
--- juliads/setup-juliads.sh
+++ juliads/setup-juliads.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 # install julia
 
-wget -q https://julialang-s3.julialang.org/bin/linux/x64/1.10/julia-1.10.5-linux-x86_64.tar.gz 
-tar -xvf julia-1.10.5-linux-x86_64.tar.gz > tar_out.out 2> tar_err.err
-sudo mv julia-1.10.5 /opt
-rm julia-1.10.5-linux-x86_64.tar.gz
-# put to paths
-echo 'export JULIA_HOME=/opt/julia-1.10.5' >> path.env
-echo 'export PATH=$PATH:$JULIA_HOME/bin' >> path.env
-# note that cron job must have path updated as well
-
-source path.env
+curl -fsSL https://install.julialang.org | sh -s -- -y
+. /home/ubuntu/.bashrc
 
 # install julia InMemoryDatasets and csv packages
 julia -q -e 'using Pkg; Pkg.add(["InMemoryDatasets","DLMReader", "PooledArrays", "Arrow", "CSV"])'
